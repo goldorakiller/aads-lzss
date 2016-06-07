1fd352fa583bc427ca64a5f317729d20f755a8b2
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2016-06-07 02:44:03 +0200

    * aads-lzss.sln: Encoding | decoding WIP
    * Encoder.cs:
    * Program.cs:
    * aads-lzss.csproj:

--- aads-lzss/Encoder.cs
+++ aads-lzss/Encoder.cs
@@ -1,11 +1,91 @@
 ﻿using System;
+using System.Linq;
 
-namespace aadslzss
+namespace LZSS
 {
-	public class Encoder
+	public sealed class Encoder
 	{
-		public Encoder ()
+		private static readonly Encoder instance = new Encoder();
+
+		static Encoder() {}
+
+		public static Encoder Instance
 		{
+			get 
+			{
+				return instance;
+			}
+		}
+
+		public string Encode(string fileContent, string dict, int bufferLength, int dictionaryLength) {
+
+			string buffer = "";
+			string output = "";
+
+			int pos = 0;
+			int offset = 0;
+
+			while (pos < fileContent.Length) {
+				var bufferEnd = pos + bufferLength <= fileContent.Length ? bufferLength : (fileContent.Length - pos);
+
+				buffer = fileContent.Substring (pos, bufferEnd);
+				if (buffer.Length < 4) {
+					buffer += new String ('\0', bufferLength - buffer.Length);
+				}
+
+				if (pos > 0) {
+					dict = dict.Substring (offset) + fileContent.Substring (pos - offset, offset);
+				}
+
+				if (!dict.Contains (buffer [0].ToString ())) {
+					output += 1 + buffer [0] + " ";
+					offset = 1;
+				} else {
+					int matchCount = 1;
+					while (matchCount < bufferLength &&
+					       dict.Contains (buffer.Substring (0, matchCount + 1))) {
+						matchCount++;
+					}
+
+					int os = dictionaryLength - dict.LastIndexOf(buffer.Substring(0, matchCount));
+					output += "0" + os + matchCount + " ";
+					offset = matchCount;
+				}
+
+				pos += offset;
+			}
+
+			return output;
+		}
+
+		public string Decode(string encoded, string dictionary)
+		{
+			string output = "";
+			int pos = 0;
+			int offset = 0;
+			int matchCount = 0;
+
+			while (pos < encoded.Length) {
+				if (encoded[pos] == '0') {
+					int.TryParse(encoded[pos + 2].ToString(), out matchCount);
+					int.TryParse(encoded[pos + 1].ToString(), out offset);
+//
+//					output += dictionary.Substring(dictionary.Length - offset, matchCount);
+//					dictionary = dictionary.Substring(matchCount) + output.Substring(output.Length - matchCount, matchCount);
+					pos += 3;
+				}
+				else if (encoded[pos] == '1') {
+					output += encoded[pos + 1];
+					dictionary = dictionary.Substring(1) + output.Last();
+					pos += 2;
+				}
+				else
+				{
+					pos++;
+				}
+			}
+
+			return output;
 		}
 	}
 }
--- aads-lzss/Program.cs
+++ aads-lzss/Program.cs
@@ -1,27 +1,40 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace LZSS
 {
-	class MainClass
+	class Program
 	{
 		public static void Main (string[] args)
 		{
 			const int _dictionaryLength = 8;
 			const int _bufferLength = 4;
 
-			char[] dictionary = new char[_dictionaryLength];
+			string dictionary;
 
-			try
-			{
-				using (StreamReader sr = new StreamReader("sam-i-am.txt")) {
-					String line = sr.ReadToEnd();
+			try {
+				StreamReader sr = new StreamReader("sam-i-am.txt", Encoding.ASCII);
+				string fileContent = sr.ReadToEnd();
+				sr.Close();
 
-					for (int i = 0; i < _dictionaryLength; i++) {
-						dictionary[i] = line[0];
-					}
-				}
-			} 
+				dictionary = new String(fileContent.FirstOrDefault(), _dictionaryLength);
+
+				string encoded = Encoder.Instance.Encode(fileContent, dictionary, _bufferLength, _dictionaryLength);
+
+				#if DEBUG
+					Console.WriteLine("Encoded: ");
+					Console.WriteLine(encoded);
+				#endif
+
+				string decoded = Encoder.Instance.Decode(encoded, dictionary);
+
+				#if DEBUG
+					Console.WriteLine("Decoded: ");
+					Console.WriteLine(decoded);
+				#endif
+			}
 			catch (IOException e) {
 				Console.WriteLine ("Couldn't read the file: " + e);
 			}
